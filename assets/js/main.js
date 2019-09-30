ce03755c2626e08eee8a59e17b14c23e82024d5c
Erwan Lagrange <lagrange.erwan@outlook.com>
2019-10-01 00:32:38 +0200

    première version

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,7 +37,58 @@ function flipBonnet2() {
           imageBonnet2.src = SelectBonnet2.children[SelectBonnet2.selectedIndex].getAttribute('url');
       }
 
+function flipYeux() {
+          imageYeux.src = SelectYeux.children[SelectYeux.selectedIndex].getAttribute('url');
+      }
+
 	var imageBonnet1 = document.getElementById('imageBonnet1');
     var SelectBonnet1 = document.getElementById('SelectBonnet1');
     var imageBonnet2 = document.getElementById('imageBonnet2');
     var SelectBonnet2 = document.getElementById('SelectBonnet2');
+    var imageYeux = document.getElementById('imageYeux');
+    var SelectYeux = document.getElementById('SelectYeux');
+
+    // varibales des infos
+    var Nom = $( "#Nom" ).val();
+    var Bonnet1 = $( "#SelectBonnet1 option:selected" ).text();
+    var Bonnet2 = $( "#SelectBonnet2 option:selected" ).text();
+    var Yeux = $( "#SelectYeux option:selected" ).text();
+
+
+    // actualisation en temps réelle des infos
+	$('#Nom').change(function(){
+   		 Nom = $( "#Nom" ).val();
+	});
+
+    $('#SelectBonnet1').change(function(){
+   		 Bonnet1 = $( "#SelectBonnet1 option:selected" ).text();
+	});
+
+	$('#SelectBonnet2').change(function(){
+   		 Bonnet2 = $( "#SelectBonnet2 option:selected" ).text();
+	});
+
+	$('#SelectYeux').change(function(){
+   		 Yeux = $( "#SelectYeux option:selected" ).text();
+	});
+
+
+	// custom le mail
+	var formemail = document.querySelector('#form-email');
+	var btnemail = document.querySelector('#btn-email');
+
+	var original_mailto_url = formemail.action;
+
+	// add the event listener to the link element
+	btnemail.addEventListener('click', function () {
+	  // assemble the new mailto URL string
+	  var mailto_url = original_mailto_url + '?subject=Demande de devis&body=Entrer votre message personnalisé : %0A%0A%0A%0A%0A%0A' +
+	  							'---------------------------------------------------------------------------%0A '+
+								'Ce message est généré automatiquement.%0A Merci de ne pas le modifier.%0A%0A '+
+							    'Nom: '+ Nom+'  %0A'+
+								'Couleur Bonnet 1: '+ Bonnet1 +' %0A'+
+								'Couleur Bonnet 2: '+ Bonnet2 +' %0A'+
+								'Couleur Yeux: '+ Yeux +' %0A'+
+								'---------------------------------------------------------------------------%0A%0A%0A%0A';
+	  formemail.action = mailto_url;
+	});
